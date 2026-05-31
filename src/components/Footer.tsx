8c766ab56d4c33874a9ab6d361a55a25da85260a
engjellkrasniqi <krasniqie786@gmail.com>
2026-05-31 22:13:10 +0200

    logo

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -10,7 +10,7 @@ export default function Footer() {
         <img
           src="/CSS.png"
           alt="Clip Scale Studio logo"
-          className="h-8 w-auto object-contain"
+          className="h-12 w-auto object-contain"
         />
       </Link>
     </footer>
